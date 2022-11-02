fd031d464584ed5dd80432d16298c9cf0230be19
Macko++ <mackomaxo@duck.com>
2022-11-02 12:52:11 -0500

    Initial working login

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import supabase from "$lib/db";
+
+	let email = "";
+	let password ="";
+
+	async function signUp() {
+		const { user, error } = await supabase.auth.signUp({
+			email: email,
+			password: password,
+		});
+		console.log(user, error);
+	};
+</script>
+
+<input type="email" bind:value={email}> <br>
+<input type="password" bind:value={password}> <br>
+
+<button on:click={signUp}>Sign Up</button>
